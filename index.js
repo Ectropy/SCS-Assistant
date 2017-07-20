2a68e7d0ddc392629b2abec8227be4c6dc5e83df
Ectropy <atomicfusion0@gmail.com>
2017-07-20 15:39:38 -0400

    Made code comply with "happiness" standard & added surf naval def logic

--- index.js
+++ index.js
@@ -1,127 +1,174 @@
-(function(window){
-    'use strict';
+(function (window) {
+	'use strict';
 
-    //Set up the selectizes.
-    $('#attackingUnit').selectize({
-    });
+	// Set up the selectizes.
+	$('#attackingUnit').selectize({
+	});
 
-    $('#defendingUnit').selectize({
-    });
+	$('#defendingUnit').selectize({
+	});
 
-    var strike;
+	var strike;
 
-    var Die1 = 2;
-    var Die2 = 3;
-    var Attacker = {
-      country:  "prc",
-      type:     "naval",
-      typeUnit: "CG Type 55",
-      move:     4,
-      stealth:  2,
-      ca:       null,
-      g:        2,
-      gRange:   0,
-      u:        2,
-      uRange:   0,
-      as:       4,
-      asRange:  6,
-      ag:       4,
-      agRange:  10,
-      md:       9,
-      amd:      null,
-      aa:       null,
-      t:        2,
-      tRange:   0,
-      steps:    3
-    };
-    var Defender = {
-      country:  "prc",
-      type:     "naval",
-      typeUnit: "CG Type 55",
-      move:     4,
-      stealth:  2,
-      ca:       null,
-      g:        2,
-      gRange:   0,
-      u:        2,
-      uRange:   0,
-      as:       4,
-      asRange:  6,
-      ag:       4,
-      agRange:  10,
-      md:       9,
-      amd:      null,
-      aa:       null,
-      t:        2,
-      tRange:   0,
-      steps:    3
-    };
-    var TicksMissing = true;
-    var AirDistance = null;
-    var CombinedArmsLocation = null;
+	var Die1 = 2;
+	var Die2 = 3;
+	var Attacker = {
+		country: 'prc',
+		type: 'surface naval',
+		typeUnit: 'CG Type 55',
+		move: 4,
+		stealth: 2,
+		ca: null,
+		g: 2,
+		gRange: 0,
+		u: 2,
+		uRange: 0,
+		as: 4,
+		asRange: 6,
+		ag: 4,
+		agRange: 10,
+		md: 9,
+		amd: null,
+		aa: null,
+		t: 2,
+		tRange: 0,
+		steps: 3
+	};
+	var Defender = {
+		country: 'prc',
+		type: 'surface naval',
+		typeUnit: 'CG Type 55',
+		move: 4,
+		stealth: 2,
+		ca: null,
+		g: 2,
+		gRange: 0,
+		u: 2,
+		uRange: 0,
+		as: 4,
+		asRange: 6,
+		ag: 4,
+		agRange: 10,
+		md: 9,
+		amd: null,
+		aa: null,
+		t: 2,
+		tRange: 0,
+		steps: 3
+	};
+	var TicksMissing = true;
+	var AirDistance = null;
+	var defTerrain = null;
 
-    combatRouter(Attacker, Defender)
+	$('#btnSubmitCombatants').on('click', readCombatants);
 
+	// TODO change Attacker and Defender hardcoded to Attacker and Defender selected from dropdowns
+	function readCombatants () {
+		console.log('readCombatants');
+		combatRouter(Attacker, Defender);
+	}
 
-    combatRouter(attacker,defender,defTerrain){
-        if (defender.type == "air"){
+	function combatRouter (attacker, defender) {
+		console.log('combatRouter');
+		// Set defense values to null
+		var AAdef = null;
+		var AGdef = null;
+		var ASdef = null;
+		var CAdef = null;
+		var Gdef = null;
+		var Tdef = null;
+		var Udef = null;
 
-        }
-        else if (defender.type == "ground"){
+		// determine what type of unit the defender is
+		if (defender.type === 'surface naval') {
+			switch (promptSurfaceNavalLocation()) {
+				case 'sea':
+					console.log('sea');
+					Gdef = 9;
+					Tdef = 9;
+					ASdef = defender.md;
+					break;
+				case 'port':
+					console.log('port');
+					ASdef = promptAMDInRange();
+					break;
+				default:
+					console.log('ERROR INVALID LOCATION');
+			}
+			showValidWeaponsSystems(attacker, AAdef, AGdef, ASdef, CAdef, Gdef, Tdef, Udef);
+		} else if (defender.type === 'submarine') {
+			switch (promptSubmarineLocation()) {
+				case 'sea':
+					console.log('sea');
+					break;
+				case 'port':
+					console.log('port');
+					break;
+				default:
+					console.log('ERROR INVALID LOCATION');
+			}
+		} else if (defender.type === 'air') {
+		} else if (defender.type === 'ground') {
+		}
+	}
 
-        }
-        else if (defender.type == "surface naval"){
-            if (defTerrain == "sea") {
-                if (attacker.as != null){
-                    $("divWeaponsSystems").show();
-                    $("btnAS").show();
-                }
-            }
-            else if (defTerrain == "port") {
+	function promptSurfaceNavalLocation () {
+		return prompt("Is the defending surface naval unit at sea or in port? (Type 'sea' or 'port')");
+	}
 
-            }
-        }
-        /*else if (defender.type == "submarine"){
-            if (defTerrain == "sea") {
-                if (attacker.as != null){
-                    $("btnAS").show();
-                }
-            }
-            else if (defTerrain == "port") {
+	function promptSubmarineLocation () {
+		return prompt("Is the defending submarine unit at sea or in port? (Type 'sea' or 'port')");
+	}
 
-            }
-        }*/
-        else if (defender.type == "fort"){
+	function promptAMDInRange () {
+		switch (prompt("Is there a unit Area Missile Defense (AMD) within range (same hex or adjacent hex) of the defending unit? (Type 'yes' or 'no')")) {
+			case 'yes':
+				return prompt('What is the AMD score of the unit providing Area Missle Defense?');
+				// break;
+			case 'no':
+				return 8;
+				// break;
+			default:
+				console.log('ERROR INVALID RESPONSE');
+		}
+	}
 
-        }
-        else if (defender.type == "airbase"){
-
-        }
-
-    }
-
-
-
-    //combatResolution(Die1,Die2,Attacker,Defender,Ticks,AirDistance,CombinedArmsLocation);
-    function combatResolution(die1, die2, attacker, defender, ticksMissing, airDistance, combinedArmsLocation){
-      strike = die1 + die2;
-      if (defender.type == "naval"){
-        if (sameHex === true){
-          strike = strike + attacker.as;
-        }
-        else if (sameHex === false){
-          strike = strike + attacker.g;
-        }
-      }
-      else if (defender.type ==  "air"){
-        strike = strike + attacker.aa;
-      }
-      else if (defender.type == "land"){
-        strike = strike + attacker.ag;
-      }
-      if (attacker.type == "air"){
-      }
-      else if (attacker.type == "combined arms"){
-      }
-    }
+	function showValidWeaponsSystems (attacker, AAdef, AGdef, ASdef, CAdef, Gdef, Tdef, Udef) {
+		if (AAdef !== null) {
+			if (attacker.aa !== null) {
+				$('#btnAA').prop('disabled', false).attr('class', 'btn btn-success');
+			}
+		}
+		if (AGdef !== null) {
+			if (attacker.ag !== null) {
+				$('#btnAG').prop('disabled', false).attr('class', 'btn btn-success');
+			}
+		}
+		if (ASdef !== null) {
+			if (attacker.as !== null) {
+				$('#btnAS').prop('disabled', false).attr('class', 'btn btn-success');
+			}
+		}
+		if (CAdef !== null) {
+			if (attacker.ca !== null) {
+				$('#btnCA').prop('disabled', false).attr('class', 'btn btn-success');
+			}
+		}
+		if (Gdef !== null) {
+			if (attacker.g !== null) {
+				$('#btnG').prop('disabled', false).attr('class', 'btn btn-success');
+			}
+		}
+		if (Tdef !== null) {
+			if (attacker.t !== null) {
+				$('#btnT').prop('disabled', false).attr('class', 'btn btn-success');
+			}
+		}
+		if (Udef !== null) {
+			if (attacker.u !== null) {
+				$('#btnU').prop('disabled', false).attr('class', 'btn btn-success');
+			}
+		}
+		$('#divWeaponsSystems').show();
+	}
 })(window);
