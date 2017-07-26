5959fe2fc40198f97aede03edcd777a2ccb8e211
Ectropy <atomicfusion0@gmail.com>
2017-07-26 12:56:23 -0400

    Improved visual style of scores and required roles

--- index.js
+++ index.js
@@ -116,10 +116,12 @@
 				default:
 					console.log('ERROR INVALID LOCATION');
 			}
+			showAttackerAttackScores ();
 			showDefenderDefenseScores();
 			showValidWeaponsSystems(Attacker, AAdef, AGdef, ASdef, CAdef, Gdef, Tdef, Udef);
 		} else if (Defender.type === 'Air') {
 			AAdef = parseFloat(Defender.md); // TODO can air be defended by area missile defense?
+			showAttackerAttackScores ();
 			showDefenderDefenseScores();
 			showValidWeaponsSystems(Attacker, AAdef, AGdef, ASdef, CAdef, Gdef, Tdef, Udef);
 		} else if (Defender.type === 'Ground') {
@@ -191,6 +193,7 @@
 		}
 		function callbackNo () {
 			// Lease ASdef unchanged.
+			showAttackerAttackScores ();
 			showDefenderDefenseScores();
 			showValidWeaponsSystems(Attacker, AAdef, AGdef, ASdef, CAdef, Gdef, Tdef, Udef);
 		}
@@ -220,6 +223,7 @@
 			if (ASdef < 10) {
 				ASdef = 10;
 			}
+			showAttackerAttackScores ();
 			showDefenderDefenseScores();
 			showValidWeaponsSystems(Attacker, AAdef, AGdef, ASdef, CAdef, Gdef, Tdef, Udef);
 		}
@@ -228,24 +232,58 @@
 			if (ASdef < 11) {
 				ASdef = 11;
 			}
+			showAttackerAttackScores ();
 			showDefenderDefenseScores();
 			showValidWeaponsSystems(Attacker, AAdef, AGdef, ASdef, CAdef, Gdef, Tdef, Udef);
 		}
 	}
 
+	function showAttackerAttackScores () {
+		var html = '<div class="col-md-12">';
+		html += '<p>The attacking unit has the following attack scores</p>';
+		html +=	'<table class="table">';
+		html += '<tr><td>Air-to-Air Missiles (AA):</td>';
+		html += '<td>' + Attacker.aa + '</td></tr>';
+		html += '<tr><td>Air-to-Ground Missiles (AG):</td>';
+		html += '<td>' + Attacker.ag + '</td></tr>';
+		html += '<tr><td>Air-to-Ship Missiles (AS):</td>';
+		html += '<td>' + Attacker.as + '</td></tr>';
+		html += '<tr><td>Combined Arms (CA):</td>';
+		html += '<td>' + Attacker.ca + '</td></tr>';
+		html += '<tr><td>Guns (G):</td>';
+		html += '<td>' + Attacker.g + '</td></tr>';
+		html += '<tr><td>Torpedos (T):</td>';
+		html += '<td>' + Attacker.t + '</td></tr>';
+		html += '<tr><td>Anti-Submarine (U):</td>';
+		html += '<td>' + Attacker.t + '</td></tr>';
+		html += '</table>';
+		$('#divAttackerAttackScores').html(html);
+		$('#divAttackerAttackScores').show();
+	}
+
 	function showDefenderDefenseScores () {
 		var html = '<div class="col-md-12">';
 		html += '<p>The defending unit has the following defense scores</p>';
-		html += '<p>AA Def:' + AAdef + '</p>';
-		html += '<p>AG Def:' + AGdef + '</p>';
-		html += '<p>AS Def:' + ASdef + '</p>';
-		html += '<p>CA Def:' + CAdef + '</p>';
-		html += '<p>G Def:' + Gdef + '</p>';
-		html += '<p>T Def:' + Tdef + '</p>';
-		html += '<p>U Def:' + Udef + '</p>';
+		html +=	'<table class="table">';
+		html += '<tr><td>AA Def:</td>';
+		html += '<td>' + AAdef + '</td></tr>';
+		html += '<tr><td>AG Def:</td>';
+		html += '<td>' + AGdef + '</td></tr>';
+		html += '<tr><td>AS Def:</td>';
+		html += '<td>' + ASdef + '</td></tr>';
+		html += '<tr><td>CA Def:</td>';
+		html += '<td>' + CAdef + '</td></tr>';
+		html += '<tr><td>G Def:</td>';
+		html += '<td>' + Gdef + '</td></tr>';
+		html += '<tr><td>T Def:</td>';
+		html += '<td>' + Tdef + '</td></tr>';
+		html += '<tr><td>U Def:</td>';
+		html += '<td>' + Udef + '</td></tr>';
+		html += '</table>';
 		$('#divDefenderDefenseScores').html(html);
 		$('#divDefenderDefenseScores').show();
 	}
+
 	function showValidWeaponsSystems (Attacker, AAdef, AGdef, ASdef, CAdef, Gdef, Tdef, Udef) {
 		var untargetable = true; // If this variable is not false by the end of this function, then the attacker has no weapons systems that can target this defender.
 
@@ -303,37 +341,37 @@
 	$('#btnAA').on('click', function () {
 		Strike = parseFloat(Attacker.aa);
 		DefScore = AAdef;
-		promptAttackerTicks(promptAirRangeCheck);
+		promptAttackerSteps(promptAirRangeCheck);
 	});
 	$('#btnAG').on('click', function () {
 		Strike = parseFloat(Attacker.ag);
 		DefScore = AGdef;
-		promptAttackerTicks(promptAirRangeCheck);
+		promptAttackerSteps(promptAirRangeCheck);
 	});
 	$('#btnAS').on('click', function () {
 		Strike = parseFloat(Attacker.as);
 		DefScore = ASdef;
-		promptAttackerTicks(promptAirRangeCheck);
+		promptAttackerSteps(promptAirRangeCheck);
 	});
 	$('#btnCA').on('click', function () {
 		Strike = parseFloat(Attacker.ca);
 		DefScore = CAdef;
-		promptAttackerTicks();
+		promptAttackerSteps();
 	});
 	$('#btnG').on('click', function () {
 		Strike = parseFloat(Attacker.g);
 		DefScore = Gdef;
-		promptAttackerTicks();
+		promptAttackerSteps();
 	});
 	$('#btnT').on('click', function () {
 		Strike = parseFloat(Attacker.t);
 		DefScore = Tdef;
-		promptAttackerTicks();
+		promptAttackerSteps();
 	});
 	$('#btnU').on('click', function () {
 		Strike = parseFloat(Attacker.u);
 		DefScore = Udef;
-		promptAttackerTicks(promptAirRangeCheck);
+		promptAttackerSteps(promptAirRangeCheck);
 	});
 
 	function promptAirRangeCheck () {
@@ -344,7 +382,7 @@
 				closeButton: false,
 				animate: false,
 				title: 'Air Range',
-				message: 'Is the Attcking unit\'s hex more than ' + maxDistance + ' hexes (1/2 attacker\'s move radius) from the defending unit\'s hex?',
+				message: 'Is the attacking unit\'s hex more than ' + maxDistance + ' hexes (1/2 attacker\'s move radius) from the defending unit\'s hex?',
 				buttons: {
 					btnYes: {
 						label: 'Yes, -1 to strike',
@@ -369,12 +407,12 @@
 		}
 	}
 
-	function promptAttackerTicks (nextStep) {
+	function promptAttackerSteps (nextStep) {
 		bootbox.dialog({
 			closeButton: false,
 			animate: false,
-			title: 'Missing Ticks',
-			message: 'Is the attacking unit missing any ticks?',
+			title: 'Missing Steps',
+			message: 'Is the attacking unit missing any steps?',
 			buttons: {
 				btnYes: {
 					label: 'Yes, -1 to strike',
@@ -400,6 +438,73 @@
 
 	function showRequiredRoll () {
 		var roll = DefScore - Strike;
-		alert('defscore ' + DefScore + ' strike ' + Strike + ' roll ' + roll + 'To inflict 1 tick of damage you must roll ' + (roll + 1));
+		var html = '';
+		html += '<div class="row">';
+		html += '    <h2>Combat Results</h2>';
+		html += '</div>';
+		html += '<div class="row">';
+		html += '    <div class="col-md-2">';
+		html += '        <div class="panel panel-default">';
+		html += '            <div class="panel-heading text-center">No damage is dealt if you roll</div>';
+		html += '            <div class="panel-body text-center">';
+		html += '                <h1>&le;' + roll + '</h1>';
+		html += '            </div>';
+		html += '        </div>';
+		html += '    </div>';
+		// Since you can't deal more steps of damage than the defending unit has,
+		// and can't roll more than 12 with 2d6 (2 six-sided dies),
+		// these if statments look for those cases.
+		if (roll <= 11 && parseFloat(Defender.steps) >= 1) {
+			html += '    <div class="col-md-2">';
+			html += '        <div class="panel panel-default">';
+			html += '            <div class="panel-heading text-center">To deal 1 step of damage, roll</div>';
+			html += '            <div class="panel-body text-center">';
+			html += '                <h1>' + (roll + 1) + '</h1>';
+			html += '            </div>';
+			html += '        </div>';
+			html += '    </div>';
+		}
+		if (roll <= 10 && parseFloat(Defender.steps) >= 2) {
+			html += '    <div class="col-md-2">';
+			html += '        <div class="panel panel-default">';
+			html += '            <div class="panel-heading text-center">To deal 2 steps of damage, roll</div>';
+			html += '            <div class="panel-body text-center">';
+			html += '                <h1>' + (roll + 2) + '</h1>';
+			html += '            </div>';
+			html += '        </div>';
+			html += '    </div>';
+		}
+		if (roll <= 9 && parseFloat(Defender.steps) >= 3) {
+			html += '    <div class="col-md-2">';
+			html += '        <div class="panel panel-default">';
+			html += '            <div class="panel-heading text-center">To deal 3 steps of damage, roll</div>';
+			html += '            <div class="panel-body text-center">';
+			html += '                <h1>' + (roll + 3) + '</h1>';
+			html += '            </div>';
+			html += '        </div>';
+			html += '    </div>';
+		}
+		if (roll <= 8 && parseFloat(Defender.steps) >= 4) {
+			html += '    <div class="col-md-2">';
+			html += '        <div class="panel panel-default">';
+			html += '            <div class="panel-heading text-center">To deal 4 steps of damage, roll</div>';
+			html += '            <div class="panel-body text-center">';
+			html += '                <h1>' + (roll + 4) + '</h1>';
+			html += '            </div>';
+			html += '        </div>';
+			html += '    </div>';
+		}
+		if (roll <= 7 && parseFloat(Defender.steps) >= 5) {
+			html += '    <div class="col-md-2">';
+			html += '        <div class="panel panel-default">';
+			html += '            <div class="panel-heading text-center">To deal 5 steps of damage, roll</div>';
+			html += '            <div class="panel-body text-center">';
+			html += '                <h1>' + (roll + 5) + '</h1>';
+			html += '            </div>';
+			html += '        </div>';
+			html += '    </div>';
+		}
+		html += '</div>';
+		$('#divRequiredRoll').html(html).show();
 	}
 })(window);
